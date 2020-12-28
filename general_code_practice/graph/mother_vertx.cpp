d0ea502657c2ec54ba9c973377482e1040340dd7
AshishVarshneyy <50491289+AshishVarshneyy@users.noreply.github.com>
2020-12-28 13:48:15 +0530

    change dir name

--- general_code_practice/graph/mother_vertx.cpp
+++ general_code_practice/graph/mother_vertx.cpp
@@ -10,23 +10,22 @@ void make_graph(vector <int> adj[], int v1, int v2){
     adj[v1].push_back(v2);
 }
 
-void DFSutil(vector<int> adj[], vector<bool> &visited, int src){
+void DFSutil(vector<int> adj[], vector<bool> visited, int src){
     visited[src]=true;
+    cout<<src<<" ";
     for(auto x:adj[src]){
         if(!visited[x]){
-            cout<<x<<" ";
             DFSutil(adj, visited, x);
         }
     }
 }
 
-int DFS(vector<int> adj[], int V){
+void DFS(vector<int> adj[], int V){
     vector<bool> visited(V, false);
 
     int v=0;
-    int cnt=1;
-
-    for(int i=0; i<V;i++){
+    DFSutil(adj, visited, 5);
+/*    for(int i=0; i<V;i++){
         if(!visited[i]){
             DFSutil(adj, visited, i);
             v=i;
@@ -40,7 +39,7 @@ int DFS(vector<int> adj[], int V){
         if(!visited[i])
             return -1;
     }
-    return v;
+    return v;*/
 }
 
 int main(){
@@ -49,7 +48,7 @@ int main(){
 
     vector<int> adj[vertx];
 
-    /*make_graph(adj, 0, 1);
+    make_graph(adj, 0, 1);
     make_graph(adj, 0, 2);
     make_graph(adj, 1, 3);
     make_graph(adj, 4, 1);
@@ -57,15 +56,6 @@ int main(){
     make_graph(adj, 5, 6);
     make_graph(adj, 5, 2);
     make_graph(adj, 6, 0);
-
-    cout<<DFS(adj, vertx, 2);*/
-
-    make_graph(adj, 0, 1);
-    make_graph(adj, 0, 2);
-    make_graph(adj, 1, 2);
-    make_graph(adj, 2, 0);
-    make_graph(adj, 2, 3);
-    make_graph(adj, 3, 3);
-
     DFS(adj, vertx);
+    //cout<<DFS(adj, vertx);
 }
