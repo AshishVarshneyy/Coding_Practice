b172b47427a40f57d42759064d6db55b1de09f97
AshishVarshneyy <50491289+AshishVarshneyy@users.noreply.github.com>
2021-01-08 23:36:12 +0530

    without extra space

--- general_code_practice/Array/Merge_Without_Extra_Space.cpp
+++ general_code_practice/Array/Merge_Without_Extra_Space.cpp
@@ -8,43 +8,40 @@ Output: 0 1 2 3 5 6 7 8 9
 */
 
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 
-int mergeArrarys(int arr1[], int arr2[], int res[], int N, int M, int k){
-    int i,j;
-    for(i=0, j=0; i<=N-1, j<=M-1;){
-        if(arr1[i]<arr2[j]){
-            res[k++]=arr1[i++];
+int mergeArrarys(int arr1[], int arr2[], int n, int m){
+    int i=n-1,j=0, swap=0;
+    while(i>=0 && j<m){
+        if(arr1[i]>=arr2[j])
+        {
+            int temp=arr1[i];
+            arr1[i]=arr2[j];
+            arr2[j]=temp;
+            swap++;
         }
-        else{
-            res[k++]=arr2[j++];
-        }
+        i--;j++;
     }
-    if(i<N){
-        for(; i<N; i++)
-            res[k++]=arr1[i];
-    }
-    if(j<M){
-        for(; i<M; i++)
-            res[k++]=arr2[j];
-    }
-    return k;
+    //cout<<swap<<"\n";
+    sort(arr1, arr1+n);
+    sort(arr2, arr2+m);
 }
 
 void print(int res[], int k){
-    for(int i=0; i<k; i++){
+    for(int i=0; i<k; i++)
         cout<<res[i]<<" ";
-    }
 }
 
 int main() {
-    int N = 4, M = 5, arr1[] = {1, 3, 5, 7}, arr2[] = {0, 2, 6, 8, 9}, k=0;
-    int resLength = N+M;
-    int res[resLength];
+    int N = 4, M = 5, arr1[] = {1, 3, 5, 7}, arr2[] = {0, 2, 6, 8, 9};
 
-    k= mergeArrarys(arr1, arr2, res, N, M, k);
-    print(res, k);
+    mergeArrarys(arr1, arr2, N, M);
+
+    print(arr1, N);
+    print(arr2, M);
+
     return 0;
 }
+
