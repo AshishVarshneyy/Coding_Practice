e39478475ae54f34976488d0941d476d27f5e864
AshishVarshneyy <50491289+AshishVarshneyy@users.noreply.github.com>
2021-01-10 23:20:34 +0530

    sort 2D vector

--- general_code_practice/sort_2D_vector.cpp
+++ general_code_practice/sort_2D_vector.cpp
@@ -1,12 +1,3 @@
-/*
-Question_link: https://leetcode.com/problems/merge-intervals/
-
-Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
-*/
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -17,12 +8,6 @@ bool sortcol(const vector<int> &v1, const vector<int> &v2){
 int main(){
     vector<vector<int>> vect {{1,3},{8,10}, {2,6},{15,18}};
     sort(vect.begin(), vect.end(), sortcol);
-
-    for(int i=vect.begin(); i<vect.end(); i++){
-        if(vect[i][1]>vect[0][i+1])
-            cout<<vect[i+1][1];
-
-    }
 
     for(auto x:vect){
         for(auto y:x){
