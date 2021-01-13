ca0d3dcf3b8e65ed59c4f89d5b31cfc13c0e09d3
AshishVarshneyy <50491289+AshishVarshneyy@users.noreply.github.com>
2021-01-13 23:30:28 +0530

    merge interval

--- general_code_practice/Array/merge_interval.cpp
+++ general_code_practice/Array/merge_interval.cpp
@@ -10,25 +10,53 @@ Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 #include<bits/stdc++.h>
 using namespace std;
 
-bool sortcol(const vector<int> &v1, const vector<int> &v2){
-    return v1[0]<v2[0];
-}
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int i;
 
-int main(){
-    vector<vector<int>> vect {{1,3},{8,10}, {2,6},{15,18}};
-    sort(vect.begin(), vect.end(), sortcol);
+        //Sorting intervals on start basis:
+        sort(intervals.begin(), intervals.end());
 
-    for(int i=vect.begin(); i<vect.end(); i++){
-        if(vect[i][1]>vect[0][i+1])
-            cout<<vect[i+1][1];
+        //Declaring result vector
+        //Pushing first interval
+        vector<vector<int> >result;
+        result.push_back(intervals[i]);
+        cout<<result[0][1];
+
+        //Evaluating result;
+        for(i = 1; i < intervals.size(); i++) {
+            int n = result.size();
+
+            //if interval[i] lie inside result[n-1]
+            if(intervals[i][0] <= result[n-1][1] && intervals[i][1] <= result[n-1][1])
+                continue;
+
+            //if starting of interval[i] lies inside result[n-1]
+            //just changing the end of result interval
+            else if(intervals[i][0] <= result[n-1][1])
+                result[n-1][1] = intervals[i][1];
+
+            //pushing back the non overlapping interval
+            else
+                result.push_back(intervals[i]);
+        }
+
+        //Returning result yay!!!
+        return result;
 
     }
 
-    for(auto x:vect){
+int main(){
+    vector<vector<int>> vect {{1,3},{8,10}, {2,6},{15,18}};
+
+    vector<vector<int> >res;
+
+    res=merge(vect);
+
+    /*for(auto x:res){
         for(auto y:x){
             cout<<y<<" ";
         }
         cout<<endl;
-    }
+    }*/
     return 0;
 }
