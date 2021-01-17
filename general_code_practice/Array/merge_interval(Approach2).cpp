d81d2d67dcc8ac0c89096273d60ac5da45421451
AshishVarshneyy <50491289+AshishVarshneyy@users.noreply.github.com>
2021-01-17 20:54:42 +0530

    merge interval aproach 2

--- general_code_practice/Array/merge_interval(Approach2).cpp
+++ general_code_practice/Array/merge_interval(Approach2).cpp
@@ -10,20 +10,46 @@ Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> merge(vector<vector<int>>& intervals) {
+stack<pair<int,int>> mergeInterval(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end());
+
+    stack<pair<int,int>> res;
+    res.push({intervals[0][0], intervals[0][1]});
+
+    for(int i=1; i<intervals.size(); i++){
+        int start1 = res.top().first;
+        int end1 = res.top().second;
+        int start2 = intervals[i][0];
+        int end2 = intervals[i][1];
+
+        if(end1<start2)
+            res.push({start2, end2});
+        else{
+            res.pop();
+            res.push({start1, end2});
+        }
+    }
+    return res;
+}
+
+void printStack(stack<pair<int,int>> res){
+    if(res.empty())
+        return;
+
+    pair<int,int> element=res.top();
+    res.pop();
+    printStack(res);
+
+    cout<<element.first<<" "<<element.second<<"\n";
+
 }
 
 int main(){
-    vector<vector<int>> vect {{1,3},{8,10}, {2,6},{15,18}};
-    vector<vector<int>>res;
+    vector<vector<int>> vect {{1,2},{2,4}, {5,8},{1,10}, {11,12}};
+    stack<pair<int,int>>res;
 
-    res=merge(vect);
+    res=mergeInterval(vect);
+    printStack(res);
 
-    for(auto rows:res){
-        for(auto element:rows){
-            cout<<element<<" ";
-        }
-        cout<<endl;
-    }
     return 0;
 }
