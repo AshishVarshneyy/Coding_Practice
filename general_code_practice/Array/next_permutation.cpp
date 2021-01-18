b35504a8e0fcd7a162bf567ae708694d08d21cd0
AshishVarshneyy <50491289+AshishVarshneyy@users.noreply.github.com>
2021-01-18 19:35:57 +0530

    next permutation

--- general_code_practice/Array/next_permutation.cpp
+++ general_code_practice/Array/next_permutation.cpp
@@ -1,3 +1,22 @@
 /*
+Question_link: https://leetcode.com/problems/next-permutation/
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+
+Example 4:
+
+Input: nums = [1]
+Output: [1]
 
 */
